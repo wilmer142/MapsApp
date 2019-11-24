3db44237808205cc7b899136786c979ffc576460
wilmer142 <wilmercollege@gmail.com>
2019-11-24 13:47:34 -0500

    Implementing add marker

---
+++ src/app/classes/marcador.class.ts
@@ -0,0 +1,16 @@
+
+
+export class Marcador {
+
+    public lat: number;
+    public lng: number;
+
+    public titulo = 'Sin título';
+    public desc = 'Sin descripción';
+
+
+    constructor(lat: number, lng: number) { 
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marcador } from '../../classes/marcador.class';
 
 @Component({
   selector: 'app-map',
@@ -7,12 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  marcadores: Marcador[] = [];
+
   lat = 6.2573115;
   lng = -75.5628717;
 
-  constructor() { }
+  constructor() {
+    const nuevoMarcador = new Marcador(this.lat, this.lng);
+    this.marcadores.push(nuevoMarcador);
+  }
 
   ngOnInit() {
   }
 
+  agregarMarcador(evento) {
+    const coords: { lat: number, lng: number} = evento.coords;
+
+    const nuevoMarcador = new Marcador(coords.lat, coords.lng);
+    this.marcadores.push(nuevoMarcador);
+  }
 }
